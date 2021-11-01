d1143080aae064199e09b0b4a6f6ea938d6f3b1f
amelmeneses <amelmeneses@gmail.com>
2021-10-31 20:03:00 -0500

    final script

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ const winningMessage = () => `Player $ {currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
 const currentPlayerTurn = () => `It's $ {currentPlayer}'s turn`;
 const winingCondition = {
-  [0, 1, 2],
+   [0, 1, 2],
    [3, 4, 5],
    [6, 7, 8],
    [0, 3, 6],
