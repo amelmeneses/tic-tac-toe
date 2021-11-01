da97bae5a868c4ec760f875d2504059412169494
amelmeneses <amelmeneses@gmail.com>
2021-10-31 20:22:01 -0500

    final correction

--- script.js
+++ script.js
@@ -6,19 +6,19 @@ const statusDisplay = document.querySelector('.game--status');
 let gameActive = true;
 let currentPlayer = "X";
 let gameState = ["", "", "", "", "", "", "", "", ""];
-const winningMessage = () => `Player $ {currentPlayer} has won!`;
+const winningMessage = () => `Player ${currentPlayer} has won!`;
 const drawMessage = () => `Game ended in a draw!`;
-const currentPlayerTurn = () => `It's $ {currentPlayer}'s turn`;
-const winingCondition = {
-   [0, 1, 2],
-   [3, 4, 5],
-   [6, 7, 8],
-   [0, 3, 6],
-   [1, 4, 7],
-   [2, 5, 8],
-   [0, 4, 8],
-   [2, 4, 6]
-};
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+const winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+];
 /*
 We set the inital message to let the players know whose turn it is
 */
